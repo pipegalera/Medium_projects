55d6f68bc7fd787aa003b40e7d9a306fdc8092e6
Pipe Galera <pipegalera@gmail.com>
2020-02-20 23:49:11 +0100

    Updated project 1

---
+++ Machine Learning to Predict Stock Prices/py_files/LSTM.py
@@ -0,0 +1,14 @@
+
+# Importing packages
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+
+# Load dataset
+
+os.chdir("C:/Users/fgm.si/github/Medium_projects/Machine Learning to Predict Stock Prices")
+
+data = "raw_data/NSE-TATAGLOBAL.csv"
+train = pd.read(data.csv)
