89a6935eab238a4d0daf77e35a608cf781301ccf
Pipe Galera <pipegalera@gmail.com>
2020-03-12 17:32:29 +0100

    Implement GRU and LSTM to predict 2 stocks

--- LSTM to predict Stock Prices/py_files/LSTM.py
+++ LSTM to predict Stock Prices/py_files/LSTM.py
@@ -1,15 +1,14 @@
 # @Author: Pipe galera
 # @Date:   04-03-2020
 # @Email:  pipegalera@gmail.com
-# @Last modified by:   Pipe galera
-# @Last modified time: 04-03-2020
+# @Last modified by:   pipegalera
+# @Last modified time: 2020-03-12T17:31:42+01:00
 
 
 
 # -*- coding: utf-8 -*-
 """
-Last Edit: Feb 23 2020
-Author: Pipe Galera / Roshan Adusumilli
+Author: Pipe Galera
 Project: Machine Learning to Predict Stock Prices
 Goal: Predict Stock Prices using a Keras LSTM model
 
@@ -26,29 +25,33 @@ Dataset: Tata Global Beverage’s past stock prices
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
 import os
-from sklearn.preprocessing import MinMaxScaler
-from keras.layers import LSTM, Dropout, Dense
+from sklearn.preprocessing import MinMaxScaler,StandardScaler
+from keras.layers import LSTM, Dropout, Dense, GRU, Bidirectional
 from keras.models import Sequential
+from sklearn.metrics import mean_squared_error
+from keras.optimizers import SGD
+from math import sqrt
 
 # Load dataset
-os.chdir("C:/Users/fgm.si/documents/GitHub/side_projects/Machine Learning to Predict Stock Prices")
+os.chdir("/Users/pipegalera/Documents/GitHub/side_projects/LSTM to predict Stock Prices")
 data = pd.read_csv("raw_data/NSE-TATAGLOBAL.csv")
 data.shape
 data.head(2)
 
 # Inverse the dataset to get the timeline right
-data.head()
 data = data.iloc[::-1]
-data.tail()
+data.head(2)
 
 # Put the date to the right format
 data.columns
 
 data["Date"] = pd.to_datetime(data["Date"])
 data.set_index("Date", inplace = True)
-data.head()
+data.head(2)
 data.shape
+"""
 # First calculate the "mid prices", or the average between the High and the low stock value
 high_prices = data.loc[:,"High"].values
 low_prices = data.loc[:,"Low"].values
@@ -62,119 +65,169 @@ plt.plot(range(data.shape[0]), mid_prices)
 plt.xlabel('Date',fontsize=18)
 plt.ylabel('Mid Price',fontsize=18)
 plt.show()
-
-# Data normalization
 """
-You need to make sure that the data behaves in similar value ranges throughout the time frame
-"""
-# Fixing the shape to a x times 1D matrix
-# We are only interested in the mid_prices
-mid_prices.shape
-data.shape
-data_array = mid_prices.reshape(mid_prices.shape[0], 1)
+# Split of train and test
+data[:"2017"].iloc[:,1:2].values.shape
+data["2018":].iloc[:,1:2].values.shape
 
-# Data normalization
-scaler = MinMaxScaler()
-data_array_scalated = scaler.fit_transform(data_array)
-data_array_scalated
+train = data[:"2017"].iloc[:,1:2].values
+test = data["2018":].iloc[:,1:2].values
 
-# Split the dataset
-"""
-The training data will be the first 75% data points of the time series and the resting 25% will be test data.We are not going to use the split_train_test of sklearn because is a time series, they are not independent events.
-"""
+# Visualization
+data["High"][:"2017"].plot(figsize = (18,9), legend = True)
+data["High"]["2018":].plot(figsize = (18,9), legend = True)
+plt.legend(["Training set - Before 2018", "Test set - After 2018"])
+plt.ylabel('High Price',fontsize=18)
+plt.title("TATAS stock price")
+plt.show()
 
+# Data normalization with MinMaxScaler
+mms = MinMaxScaler()
+train_mms = mms.fit_transform(train)
 
-mid_prices.shape[0] # Numer of observations
-train_number = round(mid_prices.shape[0]*0.75)
+# timesteps for the train set
+train_mms.shape
 
-train_data = mid_prices[:train_number]
-test_data = mid_prices[train_number:]
-
-"""
-# Incorporating timesteps into data
 X_train = []
 y_train = []
 
-for i in range(60, 2035):
+for i in range(60, train_mms.shape[0]):
     # 60 timesteps and 2035 observations
-    X_train = np.append(X_train, train_data[i-60:i, 0])
-    y_train = np.append(y_train, train_data[i,0])
-
-X_train.shape
+    X_train.append(train_mms[i-60:i, 0])
+    y_train.append(train_mms[i,0])
 
 # In order to reshape, first they have to be in arrays
-X_train.shape
-y_train.shape
 X_train, y_train = np.array(X_train), np.array(y_train)
 X_train.shape
 y_train.shape
 
-# np.reshape(a, newshape, order='C')[source]
-X_train = np.reshape(X_train, (X_train.shape[0], 1) ,1)
-"""
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+X_train.shape
+
 
 # Creating the LSTM model
-    """
-    The LSTM layer is added with the following  arguments: 50 units is the dimensionality of the    output space, return_sequences=True is necessary    for stacking LSTM layers so the consequent LSTM     layer has a three-dimensional sequence input, and   input_shape is the shape of the training dataset.
+"""
+The LSTM layer is added with the following  arguments: 50 units is thed imensionality of the output space, "return_sequences=True" is necessary    forstacking LSTM layers so the consequent LSTM layer has a three-dimensional sequence input, and "input_shape" is the shape of the training dataset.
 
-    Specifying 0.2 in the Dropout layer means that 20% of the layers will be dropped
+Specifying 0.2 in the Dropout layer means that 20% of the layers will be dropped
 
-    """
+"""
 model = Sequential()
+# First LSTM layer with Dropout regularisation
 model.add(LSTM(
     units = 50,
     return_sequences = True,
-    input_shape = (X_train.shape[1], 1),
-))
+    input_shape = (X_train.shape[1], 1)))
 model.add(Dropout(0.2))
+# Second LSTM layer
 model.add(LSTM(units=50,return_sequences=True))
 model.add(Dropout(0.2))
+# Third LSTM layer
 model.add(LSTM(units=50,return_sequences=True))
 model.add(Dropout(0.2))
+# Fourth LSTM layer
 model.add(LSTM(units=50))
 model.add(Dropout(0.2))
+# The output layer
 model.add(Dense(units=1))
+# Compiling the RNN
+model.compile(optimizer='adam',loss='mean_squared_error')
 
 """
 we fit the model to run for 100 epochs, the number of times the learning algorithm will work through the entire training set, with a batch size of 32.
 """
-model.compile(optimizer='adam',loss='mean_squared_error')
 model.fit(X_train, y_train, epochs = 100, batch_size = 32)
 
+# Calculate the number of inputs that we need
+n_inputs = len(data["High"]) - len(test) - 60
+n_inputs
 
-#####################################################
+# Calculate inputs
+inputs = data["High"][n_inputs:].values
+inputs = inputs.reshape(-1,1)
 
-"""
-We want to predict the n days ahead (foward_days) having as input the m past observed days (look_back). So, if we have an input of m past days, the network output will be the prediction for the n next days.
+# Test data normalization with MinMaxScaler
+inputs_mms = mms.transform(inputs)
+inputs_mms.shape
 
-The test data will be composed of k periods (num_periods), in which every period is a series of n days prediction
-"""
-forward_days = 10
-look_back = 40
-num_periods = 20
+# Timesteps for the Test data
+X_test = []
+for i in range(60,inputs_mms.shape[0]):
+    X_test.append(inputs_mms[i-60:i, 0])
+# In order to reshape, first they have to be in arrays
+X_test = np.array(X_test)
+X_test.shape
+# Reshape
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+X_test.shape
 
-num_neurons_FL = 128
-num_neurons_SL = 64
-epochs = 220
+# Predict prices
+pred_stock_price = model.predict(X_test)
+pred_stock_price = mms.inverse_transform(pred_stock_price)
+pred_stock_price
 
-model = Sequential()
-model.add(LSTM(
-    units = num_neurons_FL,
-    return_sequences = True,
-    input_shape = (look_back, 1),
-))
-model.add(Dropout(0.2))
 
-model.add(LSTM(
-    units = num_neurons_SL,
-    return_sequences = True,
-    input_shape = (num_neurons_FL, 1),
-))
-model.add(Dropout(0.2))
-model.add(Dense(forward_days))
+# Visualization of the results
+plt.plot(test, color = 'red', label = 'Real TATA Stock Price')
+plt.plot(pred_stock_price, color = 'blue', label = 'Predicted TATA Stock Price')
+plt.title('TATA Stock Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('TATA Stock Price')
+plt.legend()
+plt.show()
 
-model.compile(optimizer='adam',loss='mean_squared_error')
-model.fit(X_train, y_train, epochs = epochs,
-                            shuffle = True,
-                            batch_size = 2,
-                            verbose = 2)
+# Calculating accuracy
+rmse = sqrt(mean_squared_error(test, pred_stock_price))
+rmse
+
+###############################
+# Gated Recurrent Units (GRU) #
+###############################
+
+# Creating the GRU model
+
+regressorGRU = Sequential()
+# First GRU layer with Dropout regularisation
+regressorGRU.add(GRU(units=50,
+                    return_sequences=True, input_shape=(X_train.shape[1],1), activation='tanh'))
+regressorGRU.add(Dropout(0.2))
+# Second GRU layer
+regressorGRU.add(GRU(units=50,
+                    return_sequences=True, input_shape=(X_train.shape[1],1), activation='tanh'))
+regressorGRU.add(Dropout(0.2))
+# Third GRU layer
+regressorGRU.add(GRU(units=50,
+                    return_sequences=True, input_shape=(X_train.shape[1],1), activation='tanh'))
+regressorGRU.add(Dropout(0.2))
+# Fourth GRU layer
+regressorGRU.add(GRU(units=50,
+                    activation='tanh'))
+regressorGRU.add(Dropout(0.2))
+# The output layer
+regressorGRU.add(Dense(units=1))
+# Compiling the RNN
+regressorGRU.compile(optimizer=SGD(lr=0.01,
+                                    decay=1e-7,
+                                    momentum=0.9,
+                                    nesterov=False),
+                     loss='mean_squared_error')
+# Fitting to the training set
+regressorGRU.fit(X_train,y_train,epochs=3, batch_size=150)
+
+# Predict prices
+pred_stock_price_GRU = regressorGRU.predict(X_test)
+pred_stock_price_GRU = mms.inverse_transform(pred_stock_price_GRU)
+pred_stock_price_GRU
+
+# Visualization of the results
+plt.plot(test, color = 'red', label = 'Real TATA Stock Price')
+plt.plot(pred_stock_price, color = 'blue', label = 'Predicted TATA Stock Price')
+plt.title('TATA Stock Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('TATA Stock Price')
+plt.legend()
+plt.show()
+
+# Calculating accuracy
+rmse_2 = sqrt(mean_squared_error(test, pred_stock_price_GRU))
+rmse_2
---
+++ LSTM to predict Stock Prices/py_files/LSTM_2.py
@@ -0,0 +1,199 @@
+# @Author: pipegalera
+# @Date:   2020-03-12T15:30:58+01:00
+# @Last modified by:   pipegalera
+# @Last modified time: 2020-03-12T17:31:17+01:00
+
+
+
+# Importing packages
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
+import os
+from sklearn.preprocessing import MinMaxScaler,StandardScaler
+from keras.layers import LSTM, Dropout, Dense, GRU, Bidirectional
+from keras.models import Sequential
+from sklearn.metrics import mean_squared_error
+from keras.optimizers import SGD
+from math import sqrt
+
+# Load dataset
+os.chdir("/Users/pipegalera/Documents/GitHub/side_projects/LSTM to predict Stock Prices")
+data = pd.read_csv('raw_data/IBM_2006-01-01_to_2018-01-01.csv', index_col='Date', parse_dates=['Date'])
+data.shape
+data.head(2)
+
+# Split of train and test
+data[:"2016"].iloc[:,1:2].values.shape
+data["2017":].iloc[:,1:2].values.shape
+
+train = data[:"2016"].iloc[:,1:2].values
+test = data["2017":].iloc[:,1:2].values
+
+# Visualization
+data["High"][:"2016"].plot(figsize = (16,4), legend = True)
+data["High"]["2017":].plot(figsize = (16,4), legend = True)
+plt.legend(["Training set - Before 2017", "Test set - After 2017"])
+plt.ylabel('High Price',fontsize=18)
+plt.title("IBM stock price")
+plt.show()
+
+# Train data normalization with MinMaxScaler
+mms = MinMaxScaler()
+train_mms = mms.fit_transform(train)
+
+# timesteps for the Train data
+train_mms.shape
+
+X_train = []
+y_train = []
+
+for i in range(60, train_mms.shape[0]):
+    # 60 timesteps and 2035 observations
+    X_train.append(train_mms[i-60:i, 0])
+    y_train.append(train_mms[i,0])
+
+# In order to reshape, first they have to be in arrays
+X_train, y_train = np.array(X_train), np.array(y_train)
+X_train.shape
+y_train.shape
+
+# np.reshape(a, newshape, order='C')
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1],1))
+X_train.shape
+
+#########################################
+# Long Short Term Networks Model (LSTM) #
+#########################################
+
+# Creating the LSTM model
+
+"""
+The LSTM layer is added with the following  arguments: 50 units is the dimensionality of the output space, "return_sequences=True" is necessary    forstacking LSTM layers so the consequent LSTM layer has a three-dimensional sequence input, and "input_shape" is the shape of the training dataset.
+
+Specifying 0.2 in the Dropout layer means that 20% of the layers will be dropped
+
+"""
+model = Sequential()
+# First LSTM layer with Dropout regularisation
+model.add(LSTM(
+    units = 50,
+    return_sequences = True,
+    input_shape = (X_train.shape[1], 1),
+))
+model.add(Dropout(0.2))
+# Second LSTM layer
+model.add(LSTM(units=50,return_sequences=True))
+model.add(Dropout(0.2))
+# Third LSTM layer
+model.add(LSTM(units=50,return_sequences=True))
+model.add(Dropout(0.2))
+# Fourth LSTM layer
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+# The output layer
+model.add(Dense(units=1))
+
+model.compile(optimizer='rmsprop',loss='mean_squared_error')
+
+"""
+we fit the model to run for 100 epochs, the number of times the learning algorithm will work through the entire training set, with a batch size of 32.
+"""
+model.fit(X_train, y_train, epochs = 50, batch_size = 32)
+
+# We first calculate how many inputs we need
+n_inputs = len(data["High"]) - len(test) - 60
+n_inputs
+
+# Calculate the inputs
+inputs = data["High"][n_inputs:].values
+inputs = inputs.reshape(-1,1)
+inputs.shape
+
+# Test data normalization with MinMaxScaler
+inputs_mms = mms.transform(inputs)
+inputs_mms.shape
+
+# Timesteps for the Test data
+X_test = []
+for i in range(60,inputs_mms.shape[0]):
+    X_test.append(inputs_mms[i-60:i, 0])
+
+# In order to reshape, first they have to be in arrays
+X_test = np.array(X_test)
+
+# Reshape
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+
+# Prediction
+pred_stock_price = model.predict(X_test)
+pred_stock_price = mms.inverse_transform(pred_stock_price)
+pred_stock_price
+
+# Visualization of the results
+plt.plot(test, color='red',label='Real IBM Stock Price')
+plt.plot(pred_stock_price, color='blue',label='Predicted IBM Stock Price')
+plt.title('IBM Stock Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('IBM Stock Price')
+plt.legend()
+plt.show()
+
+# Calculating accuracy
+rmse = sqrt(mean_squared_error(test, pred_stock_price))
+rmse
+
+
+###############################
+# Gated Recurrent Units (GRU) #
+###############################
+
+# Creating the GRU model
+
+regressorGRU = Sequential()
+# First GRU layer with Dropout regularisation
+regressorGRU.add(GRU(units=50,
+                    return_sequences=True, input_shape=(X_train.shape[1],1), activation='tanh'))
+regressorGRU.add(Dropout(0.2))
+# Second GRU layer
+regressorGRU.add(GRU(units=50,
+                    return_sequences=True, input_shape=(X_train.shape[1],1), activation='tanh'))
+regressorGRU.add(Dropout(0.2))
+# Third GRU layer
+regressorGRU.add(GRU(units=50,
+                    return_sequences=True, input_shape=(X_train.shape[1],1), activation='tanh'))
+regressorGRU.add(Dropout(0.2))
+# Fourth GRU layer
+regressorGRU.add(GRU(units=50,
+                    activation='tanh'))
+regressorGRU.add(Dropout(0.2))
+# The output layer
+regressorGRU.add(Dense(units=1))
+# Compiling the RNN
+regressorGRU.compile(optimizer=SGD(lr=0.01,
+                                    decay=1e-7,
+                                    momentum=0.9,
+                                    nesterov=False),
+                     loss='mean_squared_error')
+# Fitting to the training set
+regressorGRU.fit(X_train,y_train,epochs=3, batch_size=150)
+
+
+# Predict prices
+pred_stock_price_GRU = regressorGRU.predict(X_test)
+pred_stock_price_GRU = mms.inverse_transform(pred_stock_price_GRU)
+pred_stock_price_GRU
+
+# Visualization of the results
+plt.plot(test, color='red',label='Real IBM Stock Price')
+plt.plot(pred_stock_price_GRU, color='blue',label='Predicted IBM Stock Price')
+plt.title('IBM Stock Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('IBM Stock Price')
+plt.legend()
+plt.show()
+
+# Calculating accuracy
+rmse_2 = sqrt(mean_squared_error(test, pred_stock_price_GRU))
+rmse_2
