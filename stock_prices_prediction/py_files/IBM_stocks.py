b3e762243d1dd138d8e9a9924c6b0a0683ed4207
Pipe Galera <pipegalera@gmail.com>
2020-04-05 21:45:13 +0200

    New and better figures

--- stock_prices_prediction/py_files/IBM_stocks.py
+++ stock_prices_prediction/py_files/IBM_stocks.py
@@ -1,4 +1,4 @@
-# @Author: pipegalera
+GRU_rmse# @Author: pipegalera
 # @Date:   2020-03-12T15:30:58+01:00
 # @Last modified by:   Pipe galera
 # @Last modified time: 05-04-2020
@@ -9,6 +9,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.dates import YearLocator, MonthLocator, DateFormatter
+import datetime
 plt.style.use('seaborn-deep')
 import os
 from sklearn.preprocessing import MinMaxScaler,StandardScaler
@@ -19,7 +21,7 @@ from keras.optimizers import SGD
 from math import sqrt
 
 # Load dataset
-os.chdir("/Users/fgm.si/Documents/GitHub/side_projects/LSTM to predict Stock Prices")
+os.chdir("/Users/fgm.si/Documents/GitHub/side_projects/stock_prices_prediction")
 data = pd.read_csv('raw_data/IBM_2006-01-01_to_2018-01-01.csv', index_col='Date', parse_dates=['Date'])
 data.shape
 data.head(2)
@@ -32,12 +34,16 @@ train = data[:"2016"].iloc[:,1:2].values
 test = data["2017":].iloc[:,1:2].values
 
 # Visualization
-data["High"][:"2016"].plot(figsize = (16,4), legend = True)
-data["High"]["2017":].plot(figsize = (16,4), legend = True)
-plt.legend(["Training set - Before 2017", "Test set - After 2017"])
-plt.ylabel('High Price',fontsize=18)
-plt.title("IBM stock price")
-plt.show()
+fig, ax = plt.subplots(figsize=(16, 4))
+ax.plot(data["High"][:"2016"], label="Training set - 2006 to 2017")
+ax.plot(data["High"]["2017":], label= "Test set - After 2017")
+ax.legend(loc="upper right")
+ax.set(title = "IBM stock price, from 2006 to 2018",
+       xlabel = "Date",
+       ylabel = "High stock price of the day")
+ax.xaxis.set_major_locator(YearLocator())
+ax.xaxis.set_major_formatter(DateFormatter('%Y'))
+plt.savefig("figures/ibm_stock_price")
 
 # Train data normalization with MinMaxScaler
 mms = MinMaxScaler()
@@ -72,35 +78,35 @@ X_train.shape
 """
 The LSTM layer is added with the following  arguments: 50 units is the dimensionality of the output space, "return_sequences=True" is necessary    forstacking LSTM layers so the consequent LSTM layer has a three-dimensional sequence input, and "input_shape" is the shape of the training dataset.
 
-Specifying 0.2 in the Dropout layer means that 20% of the layers will be dropped
+Specifying 0.2 in the Dropout layer means that 20% of the layers will be dropped, to avoid vanishin or exploding gradients.
 
 """
-model = Sequential()
+LSTM_model = Sequential()
 # First LSTM layer with Dropout regularisation
-model.add(LSTM(
+LSTM_model.add(LSTM(
     units = 50,
     return_sequences = True,
     input_shape = (X_train.shape[1], 1),
 ))
-model.add(Dropout(0.2))
+LSTM_model.add(Dropout(0.2))
 # Second LSTM layer
-model.add(LSTM(units=50,return_sequences=True))
-model.add(Dropout(0.2))
+LSTM_model.add(LSTM(units=50,return_sequences=True))
+LSTM_model.add(Dropout(0.2))
 # Third LSTM layer
-model.add(LSTM(units=50,return_sequences=True))
-model.add(Dropout(0.2))
+LSTM_model.add(LSTM(units=50,return_sequences=True))
+LSTM_model.add(Dropout(0.2))
 # Fourth LSTM layer
-model.add(LSTM(units=50))
-model.add(Dropout(0.2))
+LSTM_model.add(LSTM(units=50))
+LSTM_model.add(Dropout(0.2))
 # The output layer
-model.add(Dense(units=1))
+LSTM_model.add(Dense(units=1))
 
-model.compile(optimizer='rmsprop',loss='mean_squared_error')
+LSTM_model.compile(optimizer='rmsprop',loss='mean_squared_error')
 
 """
-we fit the model to run for 50 epochs, the number of times the learning algorithm will work through the entire training set, with a batch size of 32.
+we fit the LSTM_model to run for 50 epochs, the number of times the learning algorithm will work through the entire training set, with a batch size of 32.
 """
-model.fit(X_train, y_train, epochs = 50, batch_size = 32)
+LSTM_model.fit(X_train, y_train, epochs = 50, batch_size = 32)
 
 # We first calculate how many inputs we need
 n_inputs = len(data["High"]) - len(test) - 60
@@ -127,22 +133,45 @@ X_test = np.array(X_test)
 X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
 
 # Prediction
-pred_stock_price = model.predict(X_test)
-pred_stock_price = mms.inverse_transform(pred_stock_price)
-pred_stock_price
+LSTM_pred_stock_price = LSTM_model.predict(X_test)
+LSTM_pred_stock_price = mms.inverse_transform(LSTM_pred_stock_price)
 
 # Visualization of the results
+
+# First, we index test and the prediction values with the last days of the dataset.
+pred_days = LSTM_pred_stock_price.shape[0]
+index_days = data.index[-pred_days:]
+
+LSTM_pred_stock_price_df  = pd.DataFrame(LSTM_pred_stock_price)
+LSTM_pred_stock_price_df = LSTM_pred_stock_price_df.set_index(index_days)
+
+test_df = pd.DataFrame(test)
+test_df = test_df.set_index(data.index[-pred_days:])
+test_df
+
+# Now we plot the results with the correct date axis
+fig, ax = plt.subplots(figsize=(16, 4))
+ax.plot(test_df, color='red', label='Real IBM Stock Price')
+ax.plot(LSTM_pred_stock_price_df, color='blue',label='Predicted IBM Stock Price')
+ax.legend(loc="upper right")
+ax.set(title = 'IBM Stock Price Prediction using LSTM',
+       xlabel = "Date",
+       ylabel = "High stock price of the day")
+plt.savefig("figures/LSTM_ibm_stock_price_pred.jpg")
+
+
 plt.plot(test, color='red',label='Real IBM Stock Price')
-plt.plot(pred_stock_price, color='blue',label='Predicted IBM Stock Price')
+plt.plot(LSTM_pred_stock_price, color='blue',label='Predicted IBM Stock Price')
 plt.title('IBM Stock Price Prediction')
 plt.xlabel('Time')
 plt.ylabel('IBM Stock Price')
 plt.legend()
 #plt.show()
 plt.savefig("figures/LSTM_ibm_stock_price_pred.jpg")
+
 # Calculating accuracy
-rmse = sqrt(mean_squared_error(test, pred_stock_price))
-rmse
+LSTM_rmse = sqrt(mean_squared_error(test, LSTM_pred_stock_price))
+round(LSTM_rmse, 2)
 
 
 ###############################
@@ -151,61 +180,68 @@ rmse
 
 # Creating the GRU model
 
-regressorGRU = Sequential()
+GRU_model = Sequential()
 # First GRU layer with Dropout regularisation
-regressorGRU.add(GRU(units=50,
+GRU_model.add(GRU(units=50,
                     return_sequences=True, input_shape=(X_train.shape[1],1), activation='tanh'))
-regressorGRU.add(Dropout(0.2))
+GRU_model.add(Dropout(0.2))
 # Second GRU layer
-regressorGRU.add(GRU(units=50,
+GRU_model.add(GRU(units=50,
                     return_sequences=True, input_shape=(X_train.shape[1],1), activation='tanh'))
-regressorGRU.add(Dropout(0.2))
+GRU_model.add(Dropout(0.2))
 # Third GRU layer
-regressorGRU.add(GRU(units=50,
+GRU_model.add(GRU(units=50,
                     return_sequences=True, input_shape=(X_train.shape[1],1), activation='tanh'))
-regressorGRU.add(Dropout(0.2))
+GRU_model.add(Dropout(0.2))
 # Fourth GRU layer
-regressorGRU.add(GRU(units=50,
+GRU_model.add(GRU(units=50,
                     activation='tanh'))
-regressorGRU.add(Dropout(0.2))
+GRU_model.add(Dropout(0.2))
 # The output layer
-regressorGRU.add(Dense(units=1))
+GRU_model.add(Dense(units=1))
 # Compiling the RNN
-regressorGRU.compile(optimizer=SGD(lr=0.01,
+GRU_model.compile(optimizer=SGD(lr=0.01,
                                     decay=1e-7,
                                     momentum=0.9,
                                     nesterov=False),
                      loss='mean_squared_error')
 # Fitting to the training set
-regressorGRU.fit(X_train,y_train,epochs=50, batch_size=32)
+GRU_model.fit(X_train,y_train,epochs=50, batch_size=32)
 
 # Predict prices
-pred_stock_price_GRU = regressorGRU.predict(X_test)
+pred_stock_price_GRU = GRU_model.predict(X_test)
 pred_stock_price_GRU = mms.inverse_transform(pred_stock_price_GRU)
 pred_stock_price_GRU
 
 # Visualization of the results
-plt.plot(test, color='red',label='Real IBM Stock Price')
-plt.plot(pred_stock_price_GRU, color='blue',label='Predicted IBM Stock Price')
-plt.title('IBM Stock Price Prediction')
-plt.xlabel('Time')
-plt.ylabel('IBM Stock Price')
-plt.legend()
+
+# First, we index test and the prediction values with the last days of the dataset.
+pred_stock_price_GRU_df  = pd.DataFrame(pred_stock_price_GRU)
+pred_stock_price_GRU_df = pred_stock_price_GRU_df.set_index(index_days)
+
+# Now we plot the results with the correct date axis
+fig, ax = plt.subplots(figsize=(16, 4))
+ax.plot(test_df, color='red', label='Real IBM Stock Price')
+ax.plot(pred_stock_price_GRU_df, color='green',label='Predicted IBM Stock Price')
+ax.legend(loc="upper right")
+ax.set(title = 'IBM Stock Price Prediction using GRU',
+       xlabel = "Date",
+       ylabel = "High stock price of the day")
 plt.savefig("figures/GRU_ibm_stock_price_pred.jpg")
 
-# Calculating accuracy
-rmse_2 = sqrt(mean_squared_error(test, pred_stock_price_GRU))
-round(rmse_2, 2)
 
-# Comparing both results
-plt.gca().set_facecolor('xkcd:white')
-plt.style.use('tableau-colorblind10')
-plt.plot(test, color='red',label='Real IBM Stock Price')
-plt.plot(pred_stock_price_GRU, color='blue',label='LSTM Predicted IBM Stock Price: RMSE = 2.50')
-plt.plot(pred_stock_price, color='green',label='GRU Predicted IBM Stock Price: RMSE = 3.27')
-plt.title('IBM Stock Price Prediction')
-plt.xlabel('Time')
-plt.ylabel('IBM Stock Price')
-plt.legend()
-plt.savefig("figures/combined_ibm_stock_price_pred.jpg")
-plt.savefig("figures/combined_ibm_stock_price_pred.png")
+# Calculating accuracy
+GRU_rmse = sqrt(mean_squared_error(test, pred_stock_price_GRU))
+round(GRU_rmse, 2)
+
+# Visualization of both results
+
+fig, ax = plt.subplots(figsize=(16, 4))
+ax.plot(test_df, color='red', label='Real IBM Stock Price')
+ax.plot(pred_stock_price_GRU_df, color='green',label='Predicted IBM Stock Price using GRU. RMSE = 3.23')
+ax.plot(LSTM_pred_stock_price_df, color='blue',label='Predicted IBM Stock Price using LSTM. RMSE = 3.01')
+ax.legend(loc="upper right")
+ax.set(title = 'IBM Stock Price Prediction',
+       xlabel = "Date",
+       ylabel = "High stock price of the day")
+plt.savefig("figures/stock_price_pred.jpg")
